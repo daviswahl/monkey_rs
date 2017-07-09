38b0c503e7cd894ae94a8f8555db5bc2f6f514f1
Davis Wahl <dwahl@signalpath.com>
2017-07-08 20:24:24 -0400

    Conditional expressions

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -28,23 +28,27 @@ fn eval_bang_prefix_op_exp<'a>(obj: &Object) -> ObjectResult<'a> {
 
 fn eval_infix_expression<'a>(op: &str, left: &Object, right: &Object) -> ObjectResult<'a> {
     let result = match (left, right) {
-        (&Object::Integer(l), &Object::Integer(r)) => match op {
-            "/" => Object::Integer(l / r),
-            "*" => Object::Integer(l * r),
-            "-" => Object::Integer(l - r),
-            "+" => Object::Integer(l + r),
+        (&Object::Integer(l), &Object::Integer(r)) => {
+            match op {
+                "/" => Object::Integer(l / r),
+                "*" => Object::Integer(l * r),
+                "-" => Object::Integer(l - r),
+                "+" => Object::Integer(l + r),
 
-            ">" => Object::Boolean(l > r),
-            "<" => Object::Boolean(l < r),
-            "==" => Object::Boolean(l == r),
-            "!=" => Object::Boolean(l != r),
-            _ => Object::Null,
-        },
-        (&Object::Boolean(l), &Object::Boolean(r)) => match op {
-            "==" => Object::Boolean(l == r),
-            "!=" => Object::Boolean(l != r),
-            _ => Object::Null
-        },
+                ">" => Object::Boolean(l > r),
+                "<" => Object::Boolean(l < r),
+                "==" => Object::Boolean(l == r),
+                "!=" => Object::Boolean(l != r),
+                _ => Object::Null,
+            }
+        }
+        (&Object::Boolean(l), &Object::Boolean(r)) => {
+            match op {
+                "==" => Object::Boolean(l == r),
+                "!=" => Object::Boolean(l != r),
+                _ => Object::Null,
+            }
+        }
         _ => Object::Null,
     };
 
@@ -57,6 +61,13 @@ fn eval_minus_prefix_op_exp<'a>(obj: &Object) -> ObjectResult<'a> {
         _ => Object::Null,
     };
     Ok(result)
+}
+
+fn is_truthy(obj: &Object) -> bool {
+    match obj {
+        &Object::Boolean(true) => true,
+        _ => false,
+    }
 }
 
 impl<'a> Evaluator {
@@ -76,7 +87,26 @@ impl<'a> Evaluator {
                 let right = self.visit_expr(&*infix.right, env)?;
                 eval_infix_expression(infix.operator.as_str(), &left, &right)
             }
-            _ => Err("unimplemented")
+
+            If(ref ifexp) => self.visit_cond_expression(ifexp, env),
+
+            _ => Err("unimpl"),
+        }
+    }
+
+    fn visit_cond_expression(
+        &mut self,
+        ifexp: &ast::IfExpression,
+        env: &Environment,
+    ) -> ObjectResult<'a> {
+        let cond = self.visit_expr(&*ifexp.condition, env)?;
+
+        if is_truthy(&cond) {
+            self.visit_statement(&*ifexp.consequence, env)
+        } else if let Some(ref alt) = ifexp.alternative {
+            self.visit_statement(&**alt, env)
+        } else {
+            Ok(Object::Null)
         }
     }
 
@@ -89,7 +119,7 @@ impl<'a> Evaluator {
         match op {
             "!" => eval_bang_prefix_op_exp(right),
             "-" => eval_minus_prefix_op_exp(right),
-            _ => Err("Unimplemented")
+            _ => Err("Unimplemented"),
         }
     }
 
@@ -98,21 +128,41 @@ impl<'a> Evaluator {
             outer: None,
             env: HashMap::new(),
         };
-        let mut result = Err("");
+        let mut result = Object::Null;
         for stmt in n.statements.iter() {
             if let &ast::Node::Statement(ref s) = stmt {
-                result = self.visit_statement(&s, &env);
+                result = self.visit_statement(s, &env)?;
             }
         }
 
-        result
+        Ok(result)
+    }
+
+
+    fn visit_block_statement(
+        &mut self,
+        block: &ast::BlockStatement,
+        env: &Environment,
+    ) -> ObjectResult<'a> {
+        let mut result = Object::Null;
+        for stmt in block.statements.iter() {
+            if let &ast::Node::Statement(ref s) = stmt {
+                result = self.visit_statement(&s, &env)?;
+            }
+        }
+        Ok(result)
     }
 
     fn visit_statement(&mut self, stmt: &ast::Statement, env: &Environment) -> ObjectResult<'a> {
         use ast::Statement::*;
         match *stmt {
             Expression(ref expr) => self.visit_expr(&expr.value, env),
-            _ => Err("unimplemented")
+            Block(ref block) => self.visit_block_statement(block, env),
+
+            ref x => {
+                println!("{:?} is unimplemented", x);
+                Err("unimplemented")
+            }
         }
     }
 }
@@ -128,6 +178,24 @@ pub fn eval<'a, 'b>(node: &'a ast::Node) -> ObjectResult<'b> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    #[test]
+    fn test_if_else_exp() {
+        let tests = vec![
+            ("if (true) { 10 }", Object::Integer(10)),
+            ("if (false) { 10 }", Object::Null),
+            ("if (1 == 1) { 10 }", Object::Integer(10)),
+            ("if (1 < 2) { 10 }", Object::Integer(10)),
+            ("if (1 > 2) { 10 }", Object::Null),
+            ("if (1 > 2) { 10 } else { 20 }", Object::Integer(20)),
+            ("if (1 < 2) { 10 } else { 20 }", Object::Integer(10)),
+        ];
+
+        for t in tests {
+            let evaluated = assert_eval(t.0);
+            assert_object(evaluated, t.1);
+        }
+    }
+
     #[test]
     fn test_eval_int() {
         let tests = vec![
@@ -154,25 +222,27 @@ mod tests {
 
     #[test]
     fn test_eval_bool() {
-        let tests = vec![("true", true), ("false", false),
-                         ("1 < 2", true),
-                         ("1 > 2", false),
-                         ("1 < 1", false),
-                         ("1 > 1", false),
-                         ("1 == 1", true),
-                         ("1 != 1", false),
-                         ("1 == 2", false),
-                         ("1 != 2", true),
+        let tests = vec![
+            ("true", true),
+            ("false", false),
+            ("1 < 2", true),
+            ("1 > 2", false),
+            ("1 < 1", false),
+            ("1 > 1", false),
+            ("1 == 1", true),
+            ("1 != 1", false),
+            ("1 == 2", false),
+            ("1 != 2", true),
 
-                         ("true == true", true),
-                         ("false == false", true),
-                         ("true != false", true),
-                         ("true == false", false),
-                         ("false != true", true),
-                         ("(1 < 2) == true", true),
-                         ("(1 < 2) == false", false),
-                         ("(1 > 2) == true", false),
-                         ("(1 > 2) == false", true),
+            ("true == true", true),
+            ("false == false", true),
+            ("true != false", true),
+            ("true == false", false),
+            ("false != true", true),
+            ("(1 < 2) == true", true),
+            ("(1 < 2) == false", false),
+            ("(1 > 2) == true", false),
+            ("(1 > 2) == false", true),
         ];
 
         for t in tests {
@@ -194,6 +264,15 @@ mod tests {
         for t in tests {
             let evaluated = assert_eval(t.0);
             assert_bool(evaluated, t.1)
+        }
+    }
+
+    fn assert_object(obj: Object, expect: Object) {
+        match (obj, expect) {
+            (Object::Boolean(l), Object::Boolean(r)) => assert_eq!(l, r),
+            (Object::Integer(l), Object::Integer(r)) => assert_eq!(l, r),
+            (Object::Null, Object::Null) => assert!(true),
+            _ => assert!(false),
         }
     }
 
